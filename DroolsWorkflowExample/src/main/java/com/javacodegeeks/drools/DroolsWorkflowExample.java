6e85ef63fa4b0c2811cb5791360c4c33a6f59806
Huey Nguyen <HueyNg@vsp.com>
2018-03-30 21:01:07 +0100

    First commit

---
+++ DroolsWorkflowExample/src/main/java/com/javacodegeeks/drools/DroolsWorkflowExample.java
@@ -0,0 +1,31 @@
+package com.javacodegeeks.drools;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample file to launch a process.
+ */
+public class DroolsWorkflowExample {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-process");
+        	Message message = new Message();
+            message.setMessage("Rule is fired");
+            message.setStatus(Message.HELLO);
+            kSession.insert(message);
+            
+            // start a new process instance
+            kSession.startProcess("com.javacodegeeks.drools", null);    
+            kSession.fireAllRules();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+}
