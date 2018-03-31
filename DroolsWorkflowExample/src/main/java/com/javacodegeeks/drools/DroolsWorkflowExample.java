8e69488d7f6f9dd36800ce57e86803e43ed0de6e
Huey Nguyen <HueyNg@vsp.com>
2018-03-31 01:39:28 +0100

    Divergent node can now read global variable

--- DroolsWorkflowExample/src/main/java/com/javacodegeeks/drools/DroolsWorkflowExample.java
+++ DroolsWorkflowExample/src/main/java/com/javacodegeeks/drools/DroolsWorkflowExample.java
@@ -1,8 +1,13 @@
 package com.javacodegeeks.drools;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jbpm.ruleflow.instance.RuleFlowProcessInstance;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.ProcessInstance;
 
 /**
  * This is a sample file to launch a process.
@@ -23,11 +28,23 @@ public class DroolsWorkflowExample {
 String consumerId ="CIA";
 kSession.setGlobal("consumerId", consumerId);
             
-            kSession.insert(message);
+//            kSession.insert(message);
             
             // start a new process instance
-            kSession.startProcess("com.javacodegeeks.drools", null);    
-            kSession.fireAllRules();
+//            kSession.startProcess("com.javacodegeeks.drools", null);    
+//            kSession.fireAllRules();
+            
+            
+Map<String, Object> inputParams = new HashMap<String, Object>();
+inputParams.put("consumerId", consumerId);
+
+ ProcessInstance processInstance= kSession.startProcess("com.javacodegeeks.drools", inputParams);  
+// RuleFlowProcessInstance ruleFlowProcessInstance= (RuleFlowProcessInstance) processInstance;
+// ruleFlowProcessInstance.setVariable("consumerId",consumerId);
+
+ 
+ 			kSession.insert(message);
+    		kSession.fireAllRules();            
         } catch (Throwable t) {
             t.printStackTrace();
         }
