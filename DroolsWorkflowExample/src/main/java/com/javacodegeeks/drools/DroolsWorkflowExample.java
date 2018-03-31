113da9d76d10b30236e922ebd6c01c20dce8dbcc
Huey Nguyen <HueyNg@vsp.com>
2018-03-31 01:45:41 +0100

    Divergent node is now correctly choosing rule path.

--- DroolsWorkflowExample/src/main/java/com/javacodegeeks/drools/DroolsWorkflowExample.java
+++ DroolsWorkflowExample/src/main/java/com/javacodegeeks/drools/DroolsWorkflowExample.java
@@ -14,40 +14,30 @@ import org.kie.api.runtime.process.ProcessInstance;
  */
 public class DroolsWorkflowExample {
 
-    public static final void main(String[] args) {
-        try {
-            // load up the knowledge base
-	        KieServices ks = KieServices.Factory.get();
-    	    KieContainer kContainer = ks.getKieClasspathContainer();
-        	KieSession kSession = kContainer.newKieSession("ksession-process");
-        	Message message = new Message();
-            message.setMessage("Rule is fired");
-            message.setStatus(Message.HELLO);
-            
-            
-String consumerId ="CIA";
-kSession.setGlobal("consumerId", consumerId);
-            
-//            kSession.insert(message);
-            
-            // start a new process instance
-//            kSession.startProcess("com.javacodegeeks.drools", null);    
-//            kSession.fireAllRules();
-            
-            
-Map<String, Object> inputParams = new HashMap<String, Object>();
-inputParams.put("consumerId", consumerId);
+	public static final void main(String[] args) {
+		try {
+			// load up the knowledge base
+			KieServices ks = KieServices.Factory.get();
+			KieContainer kContainer = ks.getKieClasspathContainer();
+			KieSession kSession = kContainer.newKieSession("ksession-process");
+			Message message = new Message();
+			message.setMessage("Rule is fired");
+			message.setStatus(Message.HELLO);
 
- ProcessInstance processInstance= kSession.startProcess("com.javacodegeeks.drools", inputParams);  
-// RuleFlowProcessInstance ruleFlowProcessInstance= (RuleFlowProcessInstance) processInstance;
-// ruleFlowProcessInstance.setVariable("consumerId",consumerId);
+//			String consumerId = "CIA";
+			String consumerId = "EYE_FINITY";			
+			kSession.setGlobal("consumerId", consumerId);
 
- 
- 			kSession.insert(message);
-    		kSession.fireAllRules();            
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
-    }
+			Map<String, Object> inputParams = new HashMap<String, Object>();
+			inputParams.put("consumerId", consumerId);
+
+			kSession.startProcess("com.javacodegeeks.drools", inputParams);
+			kSession.insert(message);
+			kSession.fireAllRules();
+			
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
 
 }
